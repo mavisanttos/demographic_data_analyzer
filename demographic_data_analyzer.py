12fcc2a56a95ccb98c23a2dc20f10b79c238e3f3
Maria Vitória <maria.santos@sou.inteli.edu.br>
2025-08-20 21:10:12 -0300

    teste

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,10 +3,6 @@ import pandas as pd # importa a biblioteca Pandas
 def calculate_demographic_data(print_data=True): # cria uma função
     # leitura dos dados
     df = pd.read_csv('adult.data.csv')
-    # limpeza dos dados
-    df = df[df['workclass'] != '?']
-    df = df[df['occupation'] != '?']
-    df = df[df['native-country'] != '?']
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
     race_count = df['race'].value_counts()
